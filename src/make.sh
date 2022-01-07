f17a1b0486816099d3840ab08ebd58d3b61a48e4
Trey White <trey.white@hpe.com>
2022-01-06 20:04:30 -0500

    Successful build and run on Crusher.

---
+++ src/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+module load rocm
+module list
+set -x
+export CXX='hipcc'
+export CXXFLAGS='-g -O3 -Wall --offload-arch=gfx90a'
+export CPPFLAGS="-DHAVE_MPI -DHAVE_ASYNC_MPI -I${MPICH_DIR}/include"
+export LDFLAGS="-L${MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx90a} ${PE_MPICH_GTL_LIBS_amd_gfx90a}"
+make clean
+make -j
